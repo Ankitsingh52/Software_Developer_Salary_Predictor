4a09b45d5d6ae9ba3fdb895d96dae5e2e3f8a38b
Ankit Singh <89691977+Ankitsingh52@users.noreply.github.com>
2023-08-10 20:30:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 s = "Salary Predictor"
-st.set_page_config(page_title=s,page_icon="icon2.jpeg")
+st.set_page_config(page_title=s, page_icon="icon2.jpeg")
 import sys
 
 sys.path.insert(1, "/Users/ankit/opt/anaconda3/envs/ml/lib/python3.9/site-packages/streamlit_option_menu")
@@ -10,32 +10,29 @@ import base64
 from predict import show_predict_page
 from explore import show_explore_page
 
-#background img/gif
-
+# background img/gif
 def add_bg_from_local(image_file):
     with open(image_file, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
     st.markdown(
-    f"""
-    <style>
-    .stApp {{
-        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
-        background-size: cover
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
+        f"""
+        <style>
+        .stApp {{
+            background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+            background-size: cover
+        }}
+        </style>
+        """,
+        unsafe_allow_html=True
     )
 
 add_bg_from_local('SDE5.jpeg')
 
-selected = option_menu(
-        menu_title = "Main Menu",
-        options = ["Predict","Explore"],
-        icons = ["lightbulb-fill","book"],
-        menu_icon = "cast",
-        default_index = 0,
-        orientation = "horizontal",
+# Sidebar navigation
+selected = st.sidebar.radio(
+    "Navigation",
+    ["Predict", "Explore"],
+    index=0
 )
 
 if selected == "Predict":
@@ -43,32 +40,31 @@ if selected == "Predict":
 else:
     show_explore_page()
 
-#To hide water mark
-
+# To hide water mark
 hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            </style>
-            """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    </style>
+"""
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
-#Feedbacks
+# Feedbacks
 with st.container():
     st.write("---")
-    st.write("""## Get In Touch With Me For Feedbacks/Suggestions !""")
+    st.write("## Get In Touch With Me For Feedbacks/Suggestions !")
     st.write("##")
 
     contact_form = """
     <form action="https://formsubmit.co/soulankit1234@gmail.com" method="POST">
-        <input type="text" name = "name" placeholder = "Your name" required>
-        <input type="email" name = "email" placeholder = "Your email" required>
+        <input type="text" name="name" placeholder="Your name" required>
+        <input type="email" name="email" placeholder="Your email" required>
         <textarea name="message" placeholder="Your message here" required></textarea>
         <button type="submit">Send</button>
     </form>
     <br>
     <body>
-     <center><p>Made with ❤️</p></center>
+        <center><p>Made with ❤️</p></center>
     </body>
     <center><a href="https://www.linkedin.com/in/ankit-singh-4b787a207">Connect With Me</a></center>
     <br>
@@ -77,4 +73,4 @@ with st.container():
         <center><p>The Prediction May Not Be 100% Accurate</p><center>
     </body>
     """
-    st.markdown(contact_form,unsafe_allow_html=True)
+    st.markdown(contact_form, unsafe_allow_html=True)
